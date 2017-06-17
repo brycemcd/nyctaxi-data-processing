d4c6df5e605bbd9da9ebc1fab61745816665c41c
Bryce McDonnell <bryce@bridgetownint.com>
2017-06-17 11:15:23 -0400

    mean and std dev vagaries, experimenting with removing dependencies

--- src/taxidata/core.clj
+++ src/taxidata/core.clj
@@ -4,12 +4,20 @@
             [kixi.stats.core :as stats]
             ))
 
+(declare mapify)
+
 ;NOTE: it munges the trip_type keys into any order
 
-(def filename "data/trip_2016_06-100.csv")
-(def filename-all "data/yellow_tripdata_2016-06.csv")
+(def filename100 "data/trip_2016_06-100.csv")
+(def filenameAll "data/yellow_tripdata_2016-06.csv")
 
-
+;(def mapfied-trips (mapify (read_trip_file (slurp filename))))
+;(def mapified-trips (with-open [r (clojure.java.io/input-stream filename)]
+                    ;(loop [c (.read r)]
+                      ;(if (not= c -1)
+                        ;(do
+                          ;(print (char c))
+                          ;(recur (.read r)))))))
 (def to_int #(Integer. %))
 (def to_dec #(Double. %))
 (def to_dttm #(dttm_f/parse (dttm_f/formatters :mysql) %)) ; NOTE: mysql formatter is YYYYMMDD HH:mm::ss
@@ -39,9 +47,9 @@
 
 (defn read_trip_file
   "Reads a trip file and parses it"
-  [filenm]
+  [rows]
   (map #(clojure.string/split % #",")
-       (clojure.string/split filenm #"\r\n")))
+       (clojure.string/split rows #"\r\n")))
 
 (defn convert
   "converts a string into the right value of the trip data"
@@ -57,8 +65,12 @@
                  (map vector trip_header unmapped-row)))
        rows))
 
-;(def mapfied-trips (mapify (read_trip_file (slurp filename))))
 
+; BASIC MATHS
+; The calc-functions here are designed to provide easily accessible calculations
+; useful for verifying and auditing rows later
+
+; TODO: remove the column arguments and just calc a sum on the collection
 (defn calc-sum
   "Take a file and mapify"
   ([]
@@ -77,14 +89,26 @@
    (count mapfied-trips)))
 
 (defn calc-mean
-  "calculate a simple mean"
-  [column]
-  (->> mapfied-trips (transduce (map column) stats/mean)))
+  "calculate a simple mean of a collection"
+  [coll]
+  (let [sum (apply + coll)
+        cnt (count coll)]
+    (if (pos? cnt)
+      (/ sum cnt)
+      0)))
+  ;(->> mapfied-trips (transduce (map column) stats/mean)))
 
 (defn calc-stddev
-  "calculate a simple mean"
-  [column]
-  (->> mapfied-trips (transduce (map column) stats/standard-deviation)))
+  "calculate the stddev of a collection"
+  [coll]
+  (let [avg (calc-mean coll)
+        squares (for [x coll]
+                  (let [x-avg (- x avg)]
+                    (* x-avg x-avg)))
+        total (count coll)]
+    (-> (/ (apply + squares)
+           (- total 1))
+        (Math/sqrt))))
 
 (defn extreme?
   "determines if amount is unreasonable to include in analysis. For now extreme
@@ -95,5 +119,28 @@
 
 (def extreme-tip-amount? #(extreme? :tip_amount %))
 
-; This will run in a console to find extreme values:
-;(group-by extreme-tip-amount? (map :tip_amount mapfied-trips))
+
+(defn mapify-row
+  [rows]
+  (map #(clojure.string/split % #",") rows))
+
+(defn mapified-trips
+  [fname]
+  (let [x 1]
+    (with-open [rdr (clojure.java.io/reader fname)]
+      (doseq [line (line-seq rdr)]
+        (+ x (to_int (first (mapify-row line))))))
+    (println x)))
+
+(defn lazy-file-lines
+  [file]
+  (letfn [(helper [rdr]
+            (lazy-seq
+              (if-let [line (.readLine rdr)]
+                (cons line (helper rdr))
+                (do
+                  (.close rdr)
+                  nil))))]
+    (helper (clojure.java.io/reader file))))
+
+; call with (reduce + (map to_int (map first (mapify-row (lazy-file-lines filename)))))
